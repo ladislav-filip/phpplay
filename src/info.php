92ecb4cbe2bff0f4bef90415dfcb3da38cd0732d
Ladislav Filip <ladislav.filip@gmail.com>
2020-05-23 18:12:25 +0200

    Zmena info vypisu

--- src/info.php
+++ src/info.php
@@ -1,2 +1,14 @@
+<html>
+<head>
+	<title>PHP Docker Play</title>
+</head>
+<body>
+<h2>
 <?php
 echo 'Moje promenna: ' .$_ENV["LF_VAR"] . '!';
+?>
+</h2>
+<p>Lorem ipsum atak dale...</p>
+<p>Oprava</p>
+</body>
+</html>
